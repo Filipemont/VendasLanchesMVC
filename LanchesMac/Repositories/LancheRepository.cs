a86ae72e47c31645752e344fc313da87081d3e50
filipemonteiro73@gmail.com <filipemonteiro73@gmail.com>
2023-10-29 20:43:06 -0300

    Adicao do controlador lanche e da view para o mesmo com algumas modificações no layout de apresentação da página.

---
+++ LanchesMac/Repositories/LancheRepository.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories
+{
+    public class LancheRepository : ILanchesRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches.
+            Where(p => p.isLanchePreferido).
+            Include(c => c.categoria);
+
+        public Lanche GetLancheById(int lancheid) => _context.Lanches.FirstOrDefault(l => l.LancheId == lancheid);
+    }
+}
